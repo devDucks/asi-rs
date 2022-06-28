14cfdbd7cef7106e2200a440756febae0b1143b2
Mattia Procopio <promat85@gmail.com>
2022-06-28 23:08:14 +0200

    Finish off other absolute paths

--- build.rs
+++ build.rs
@@ -18,23 +18,39 @@ fn main() {
     };
 
     if cfg!(target_os = "linux") && cfg!(target_arch = "x86") {
+        let path = std::fs::canonicalize("./lib/linux/x86");
         println!("cargo:rustc-link-search=./lib/linux/x86");
-        println!("cargo:rustc-link-arg=-Wl,-rpath,./lib/linux/x86");
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,{}",
+            path.unwrap().display()
+        );
     };
 
     if cfg!(target_os = "linux") && cfg!(target_arch = "arm") {
+        let path = std::fs::canonicalize("./lib/linux/armv7");
         println!("cargo:rustc-link-search=./lib/linux/armv7");
-        println!("cargo:rustc-link-arg=-Wl,-rpath,./lib/linux/armv7");
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,{}",
+            path.unwrap().display()
+        );
     };
 
     if cfg!(target_os = "linux") && cfg!(target_arch = "aarch64") {
+        let path = std::fs::canonicalize("./lib/linux/armv8");
         println!("cargo:rustc-link-search=./lib/linux/armv8");
-        println!("cargo:rustc-link-arg=-Wl,-rpath,./lib/linux/armv8");
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,{}",
+            path.unwrap().display()
+        );
     };
 
     if cfg!(target_os = "windows") {
+        let path = std::fs::canonicalize("./lib/windows/x64");
         println!("cargo:rustc-link-search=./lib/windows/x64");
-        println!("cargo:rustc-link-arg=-Wl,-rpath,./lib/windows/x64");
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,{}",
+            path.unwrap().display()
+        );
     };
 
     println!("cargo:rustc-link-lib=ASICamera2");
