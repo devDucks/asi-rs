5407370dbd0b4132e9a7f94443d7fb21c6707461
Mattia Procopio <promat85@gmail.com>
2026-03-18 22:23:23 +0100

    Upgrade mqttc to 0.25

--- src/bin/ccd/ccd.rs
+++ src/bin/ccd/ccd.rs
@@ -178,7 +178,7 @@ pub mod utils {
 
                     let mut mqttoptions = MqttOptions::new("asi_exposure", "127.0.0.1", 1883);
                     mqttoptions.set_keep_alive(Duration::from_secs(5));
-                    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+                    let (client, mut connection) = Client::new(mqttoptions, 10);
 
                     client
                         .publish(
